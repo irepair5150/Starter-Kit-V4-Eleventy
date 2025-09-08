d82c79e595476d04b9d8d3444ab43d2cb3b29fec
irepair5150 <60788276+irepair5150@users.noreply.github.com>
2025-09-08 13:02:28 -0400

    Update index.js

--- src/admin/index.js
+++ src/admin/index.js
@@ -2,4 +2,4 @@ import CMS from "netlify-cms-app";
 import HomePagePreview from "./preview-templates/HomePagePreview";
 
 // Register the preview for the "home" collection in config.yml
-CMS.registerPreviewTemplate("home", HomePagePreview);
+CMS.registerPreviewTemplate("pages", HomePagePreview);
