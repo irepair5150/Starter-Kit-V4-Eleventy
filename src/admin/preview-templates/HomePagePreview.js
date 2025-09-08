e2e4f3954974492bdfc4238d00ea90ddc384963e
irepair5150 <60788276+irepair5150@users.noreply.github.com>
2025-09-07 23:40:53 -0400

    Update HomePagePreview.js

--- src/admin/preview-templates/HomePagePreview.js
+++ src/admin/preview-templates/HomePagePreview.js
@@ -1,4 +1,13 @@
-import CMS from "netlify-cms-app";
-import HomePage from "../_includes/components/HomePagePreview";
+import React from "react";
 
-CMS.registerPreviewTemplate("home", HomePage);
+export default function HomePagePreview({ entry }) {
+  const title = entry.getIn(["data", "title"]);
+  const body = entry.getIn(["data", "body"]);
+
+  return (
+    <main style={{ fontFamily: "system-ui", padding: "2rem" }}>
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
+    </main>
+  );
+}
